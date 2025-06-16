776115611e0c711b3b8cbe137d1f40d6e76983d5
finnensley <finniganensley@gmail.com>
2025-06-16 16:53:26 -0600

    added html/script

---
+++ script.js
@@ -0,0 +1,22 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function addTask() {
+   // console.log("HELLO ADD");
+   const taskText = taskInput.value.trim();
+   if (taskText === "") return;
+
+   const taskItem = document.createElement("li");
+   taskItem.textContent = taskText;
+   taskItem.classList.add("task");
+
+   // add a click event to remove task
+   taskItem.addEventListener("click", function() {
+    taskList.removeChild(taskItem);
+   })
+
+   // adds item to list 
+   taskList.appendChild(taskItem);
+
+   taskInput.value = "";
+}
